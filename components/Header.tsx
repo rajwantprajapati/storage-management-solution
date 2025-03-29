f43e2392c12235197352fac7abf2dd3490a48c6b
Rajawant Prajapati <rajawant.prajapati@freecharge.com>
2025-03-29 12:45:56 +0530

    Added storage management solution pages

---
+++ components/Header.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { Button } from './ui/button'
+import Image from 'next/image'
+import Search from './Search';
+import FileUploader from './FileUploader';
+import { signOutUser } from '@/lib/actions/user.actions';
+
+interface Props {
+    userId: string,
+    accountId: string
+}
+
+const Header = ({userId, accountId}: Props) => {
+  return (
+    <header className="header">
+      <Search />
+      
+      <div className="header-wrapper">
+        <FileUploader ownerId={userId} accountId={accountId}/>
+
+        <form action={async() => {
+            "use server";
+            await signOutUser();
+        }}>
+          <Button type="submit" className="sign-out-button">
+            <Image
+              src="/assets/icons/logout.svg"
+              alt="logout"
+              width={24}
+              height={24}
+              className="w-6"
+            />
+          </Button>
+        </form>
+      </div>
+    </header>
+  );
+}
+
+export default Header
